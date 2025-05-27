81f939881e66fe5e9e061974e7ff9e9b561a1997
roshanrjl <roshanrijal700@gmail.com>
2025-05-27 09:31:20 +0545

    Implement code changes to enhance functionality and improve performance

--- DB4(JDBC)/Main.java
+++ DB4(JDBC)/Main.java
@@ -2,42 +2,43 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 1 . Setup the database connection
-        String url = "jdbc:postgresql://ep-raspy-frog-a5vzaqws-pooler.us-east-2.aws.neon.tech/neondb?user=neondb_owner&password=npg_9QvSjpMCE6kz&sslmode=require";
+        // MySQL database URL
+        String url = "jdbc:mysql://localhost:3306/student_db";
+        String username = "root";  // change if your MySQL username is different
+        String password = "";      // change if your MySQL password is set
 
         try {
-            // 2. Load the MySQL JDBC driver
-            // Class.forName("com.mysql.cj.jdbc.Driver");
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // load the PostgreSQL JDBC driver
-            try {
-                Class.forName("org.postgresql.Driver");
-            } catch (ClassNotFoundException e) {
-                System.out.println("PostgreSQL JDBC Driver not found");
-                e.printStackTrace();
-                return;
-            }
-            // 3. Establish the connection
-            Connection connection = DriverManager.getConnection(url);
+            // Establish the connection
+            Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the database!");
 
-            // 4 Create a statement
+            // Create a statement
             Statement statement = connection.createStatement();
 
-            // 5 Execute a query
-            ResultSet resultSet = statement.executeQuery("select * from playing_with_neon");
+            // Execute a query
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM student");
 
             // Process the result set
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                Float value = resultSet.getFloat("value");
-                System.out.println("ID: " + id + ", Name: " + name);
+                // Uncomment below lines if you want to fetch more fields
+                // String name = resultSet.getString("name");
+                // float value = resultSet.getFloat("value");
+                System.out.println("ID: " + id);
             }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+            // Close the connection
+            connection.close();
 
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("SQL Exception occurred");
+            e.printStackTrace();
         }
     }
 }
